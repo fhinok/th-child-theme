f5bd3aed9ff5ea5ff888566026588b547aa6b4de
Sämi Will <saemiwill@gmail.com>
2021-03-03 09:36:09 +0100

    text toggle

--- scripts.js
+++ scripts.js
@@ -1,8 +1,14 @@
 jQuery(function ($) {
+    $.fn.extend({
+        toggleText: function(a, b){
+            return this.text(this.text() == b ? a : b);
+        }
+    });
+
     $(document).ready( function() {
         $(".toggle_filters_button").on("click", () => {
-            // Text ändern
             $("#th_filters").slideToggle(300);
+            $(".toggle_filters_action").toggleText("ausblenden", "einblenden");
             $(".toggle_filters_icon").toggleClass("rotate");
         })
     });
