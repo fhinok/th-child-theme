d4c43e7a8df698fc784392930252bcb6e5b9815e
Sämi Will <saemiwill@gmail.com>
2021-04-13 11:10:50 +0200

    woocommerce update adjustments

--- woocommerce/order/order-details-item.php
+++ woocommerce/order/order-details-item.php
@@ -12,7 +12,7 @@
  *
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
- * @version 3.7.0
+ * @version 5.2.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -27,7 +27,11 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 
 	<td class="woocommerce-table__product-name product-name">
 		<?php
-		
+		$is_visible        = $product && $product->is_visible();
+		$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
+
+		echo wp_kses_post( apply_filters( 'woocommerce_order_item_name', $product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(), $item, $is_visible ) );
+			
 		$qty          = $item->get_quantity();
 		$refunded_qty = $order->get_qty_refunded_for_item( $item_id );
 
@@ -38,11 +42,6 @@ if ( ! apply_filters( 'woocommerce_order_item_visible', true, $item ) ) {
 		}
 
 		echo apply_filters( 'woocommerce_order_item_quantity_html', ' <strong class="product-quantity">' . sprintf( '%s&nbsp;&times;', $qty_display ) . '</strong>', $item ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-
-		$is_visible        = $product && $product->is_visible();
-		$product_permalink = apply_filters( 'woocommerce_order_item_permalink', $is_visible ? $product->get_permalink( $item ) : '', $item, $order );
-
-		echo apply_filters( 'woocommerce_order_item_name', $product_permalink ? sprintf( '<a href="%s">%s</a>', $product_permalink, $item->get_name() ) : $item->get_name(), $item, $is_visible ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 		do_action( 'woocommerce_order_item_meta_start', $item_id, $item, $order, false );
 
